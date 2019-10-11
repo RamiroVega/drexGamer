e4d4d3fcf2f493f03a84f979b01017cf1048cdf6
ramiro <ramiro.vega1993@gmail.com>
2019-10-11 14:52:48 -0500

    sidenav part2
    Dirreciones e iconos del sidenav se le dio mejor presentacions

--- src/componentes/Sidenav.js
+++ src/componentes/Sidenav.js
@@ -9,20 +9,25 @@ import img2 from '../img/Anthem.jpg';
              <nav className="prymaryColor"></nav>
                 <ul id="slide-out">
                     <li>
-                        <div className="user-view">
+                        <div className="user-view ">
                             <div className="background">
                                 <img src={img2} alt=""/>
                             </div>
-                            <a href="#!"><img className="circle" src={img1} alt=""/>Hola</a>
-                            <a href="#!"><span className="white-text name">John Doe</span></a>
-                            <a href="#!"><span className="white-text email">jdandturk@gmail.com</span></a>
+                            <a href="#!"><img className="circle" src={img1} alt=""/></a>
+                            <a href="#!"><span className="white-text name center"><h3>Drex Gamer</h3></span></a>
+                            <div className="container">
+                                <div className="row">
+                                    <a href="#!" className="col s6 white-text redes-sidenav"><i className="right fab fa-linkedin-in fa-2x"/></a>
+                                    <a href="#!" className="col s6 white-text redes-sidenav"><i className="left fab fa-facebook fa-2x"/></a>
+                                </div>    
+                            </div>                      
                         </div>
                     </li>
-                    <li><a href="#!"><i className="material-icons">cloud</i>First Link With Icon</a></li>
-                    <li><a href="#!">Second Link</a></li>
+                    <li><a href="/"><i className="material-icons"><i className="fas fa-newspaper"></i></i>Noticias</a></li>
                     <li><div className="divider"></div></li>
-                    <li><a className="subheader">Subheader</a></li>
-                    <li><a className="waves-effect" href="#!">Third Link With Waves</a></li>
+                    <li><a href="recomendados"><i className="small material-icons">star</i>Recomendados</a></li>
+                    <li><div className="divider"></div></li>
+                    <li><a className="waves-effect" href="proximos"><i className="small material-icons">local_shipping</i>Proximos</a></li>
                 </ul>
          </div>
      );
